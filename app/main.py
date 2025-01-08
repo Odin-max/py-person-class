5d3c2bae3237edd33b908db5e71671299df5a593
Odin-max <meh84741@gmail.com>
2025-01-08 22:38:18 +0200

    1234

--- app/main.py
+++ app/main.py
@@ -7,6 +7,7 @@ class Person:
         self.age = age
         Person.people[name] = self
 
+
 def create_person_list(people: list) -> list:
     person_list = [Person(person["name"], person["age"]) for person in people]
 
